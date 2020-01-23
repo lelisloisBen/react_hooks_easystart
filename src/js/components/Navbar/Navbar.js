e7cb5be10e076b4931cd366e5bb80b09a7817440
samir benzada <samirbenzada@gmail.com>
2020-01-23 17:11:08 -0500

    -m

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -6,20 +6,17 @@ const MyNavBar = () => {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/">Navbar</Link>
+                <Link to="/">Hello World</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <Link to="/">Home </Link>
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <Link to="/"> <button className="btn btn-primary m-1" >Home</button>  </Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="/">Features </Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/">Pricing </Link>
+                        <Link to="/demo"><button className="btn btn-primary m-1" >Demo</button></Link>
                     </li>
                     </ul>
                 </div>
